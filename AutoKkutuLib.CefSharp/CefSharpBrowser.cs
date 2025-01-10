04aa24ab767f473af0fcb5159ad5463bcfc7f9a1
Usermode <scottjsh@naver.com>
2025-01-10 09:41:24 +0900

    Add files via upload

---
+++ AutoKkutuLib.CefSharp/CefSharpBrowser.cs
@@ -0,0 +1,279 @@
+﻿using AutoKkutuLib.Browser;
+using AutoKkutuLib.CefSharp.Properties;
+using AutoKkutuLib.Properties;
+using CefSharp;
+using CefSharp.Wpf;
+using CefSharp.Wpf.Experimental;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace AutoKkutuLib.CefSharp;
+public class CefSharpBrowser : BrowserBase
+{
+	private const string BrowserConfigFile = "Browser.xml";
+	private readonly CefConfigDto config;
+	private ChromiumWebBrowser browser;
+
+	private readonly string jsbGlobalObjectName;
+	private readonly string jsbObjectName;
+
+	// Will be randomized
+	private readonly NameMapping nameRandom;
+
+	public override object? BrowserControl => browser;
+
+	public override string JavaScriptBaseNamespace { get; }
+
+	private static void SetIfAvail(string? value, Action<string> setter)
+	{
+		if (!string.IsNullOrWhiteSpace(value))
+			setter(value!);
+	}
+
+	private static string? ReplaceCd(string? str) => str?.Replace("%CURRENTDIR%", Environment.CurrentDirectory);
+
+	private static CefSettings CefConfigToCefSettings(CefConfigDto config)
+	{
+		var settings = new CefSettings();
+		SetIfAvail(ReplaceCd(config.ResourcesDirPath), s => settings.ResourcesDirPath = s);
+		SetIfAvail(ReplaceCd(config.LogFile), s => settings.LogFile = s);
+		SetIfAvail(config.JavascriptFlags, s => settings.JavascriptFlags = s);
+		SetIfAvail(config.UserAgentProduct, s => settings.UserAgentProduct = s);
+		SetIfAvail(ReplaceCd(config.LocalesDirPath), s => settings.LocalesDirPath = s);
+		SetIfAvail(config.UserAgent, s => settings.UserAgent = s);
+		SetIfAvail(config.AcceptLanguageList, s => settings.AcceptLanguageList = s);
+		SetIfAvail(config.Locale, s => settings.Locale = s);
+		SetIfAvail(config.CookieableSchemesList, s => settings.CookieableSchemesList = s);
+		SetIfAvail(ReplaceCd(config.CachePath), s => settings.CachePath = s);
+		SetIfAvail(ReplaceCd(config.RootCachePath), s => settings.RootCachePath = s);
+		settings.LogSeverity = config.LogSeverity;
+		settings.PackLoadingDisabled = config.PackLoadingDisabled;
+		settings.RemoteDebuggingPort = config.RemoteDebuggingPort;
+		settings.WindowlessRenderingEnabled = config.WindowlessRenderingEnabled;
+		settings.PersistSessionCookies = config.PersistSessionCookies;
+		settings.PersistUserPreferences = config.PersistUserPreferences;
+		settings.BackgroundColor = config.BackgroundColor;
+		settings.UncaughtExceptionStackSize = config.UncaughtExceptionStackSize;
+		settings.IgnoreCertificateErrors = config.IgnoreCertificateErrors;
+		settings.ChromeRuntime = config.ChromeRuntime;
+		settings.MultiThreadedMessageLoop = config.MultiThreadedMessageLoop;
+		settings.ExternalMessagePump = config.ExternalMessagePump;
+		settings.CookieableSchemesExcludeDefaults = config.CookieableSchemesExcludeDefaults;
+
+		if (config.CefCommandLineArgs != null)
+		{
+			foreach (var arg in config.CefCommandLineArgs)
+			{
+				if (arg.Contains('=')) // key-value type
+				{
+					var pieces = arg.Split('=', 2);
+					settings.CefCommandLineArgs?.Add(pieces[0], pieces[1]);
+					LibLogger.Debug<CefSharpBrowser>("Cef command-line argument added: {switch} = {value}", pieces[0], pieces[1]);
+				}
+				else
+				{
+					LibLogger.Debug<CefSharpBrowser>("Cef command-line switch added: {switch}", arg);
+					settings.CefCommandLineArgs?.Add(arg);
+				}
+			}
+		}
+		return settings;
+	}
+
+	public CefSharpBrowser()
+	{
+		jsbGlobalObjectName = GenerateRandomString(1677243);
+		jsbObjectName = GenerateRandomString(1677244);
+
+		try
+		{
+			if (!File.Exists(BrowserConfigFile))
+				File.WriteAllText(BrowserConfigFile, CefSharpResources.DefaultBrowserConfig);
+
+			var serializer = new XmlSerializer(typeof(CefConfigDto));
+			using var stream = File.Open(BrowserConfigFile, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+			config = (CefConfigDto?)serializer.Deserialize(stream)!;
+		}
+		catch (Exception ex)
+		{
+			LibLogger.Error<CefSharpBrowser>(ex, "Failed to reading/writing default CefSharp config.");
+			throw new IOException("CefSharp configuration file load exception", ex);
+		}
+
+		JavaScriptBaseNamespace = config.JavaScriptInjectionBaseNamespace;
+		if (string.IsNullOrWhiteSpace(JavaScriptBaseNamespace))
+			JavaScriptBaseNamespace = "window";
+
+		var settings = CefConfigToCefSettings(config);
+		try
+		{
+			if (!string.IsNullOrWhiteSpace(config.ProxyIp))
+			{
+				// https://cefsharp.github.io/api/63.0.0/html/P_CefSharp_CefSharpSettings_Proxy.htms
+				// https://stackoverflow.com/a/51888690
+				CefSharpSettings.Proxy = new ProxyOptions(config.ProxyIp, config.ProxyPort.ToString(), config.ProxyAuthUserName, config.ProxyAuthPassword);
+			}
+
+			if (!Cef.IsInitialized && !Cef.Initialize(settings, false, (IApp?)null))
+				LibLogger.Warn<CefSharpBrowser>("CefSharp initialization failed.");
+		}
+		catch (Exception ex)
+		{
+			LibLogger.Error<CefSharpBrowser>(ex, "CefSharp initialization exception.");
+		}
+
+		LibLogger.Verbose<CefSharpBrowser>("Cef settings applied.");
+
+		nameRandom = BrowserRandomNameMapping.MainHelperJs(this);
+		nameRandom.Add("___jsbGlobal___", jsbGlobalObjectName);
+		nameRandom.Add("___jsbObj___", jsbObjectName);
+		LibLogger.Debug<CefSharpBrowser>("communicatorJs + mainHelperJs name mapping: {nameRandom}", nameRandom);
+	}
+
+	public override void LoadFrontPage()
+	{
+		LibLogger.Verbose<CefSharpBrowser>("Initializing CefSharp");
+
+		browser = new ChromiumWebBrowser();
+
+		// Prevent getting detected by 'window.CefSharp' property existence checks
+		browser.JavascriptObjectRepository.Settings.JavascriptBindingApiGlobalObjectName = jsbGlobalObjectName;
+		var bindingObject = new JavaScriptBindingObject();
+		bindingObject.WebSocketSend += OnWebSocketSend;
+		bindingObject.WebSocketReceive += OnWebSocketReceive;
+		browser.JavascriptObjectRepository.Register(jsbObjectName, bindingObject);
+		browser.WpfKeyboardHandler = new WpfImeKeyboardHandler(browser); // https://github.com/cefsharp/CefSharp/issues/1262
+
+		browser.FrameLoadEnd += OnFrameLoadEnd;
+		browser.LoadError += OnLoadError;
+
+		browser.IsBrowserInitializedChanged += OnInitialized;
+
+		LibLogger.Verbose<CefSharpBrowser>("ChromiumWebBrowser instance created.");
+	}
+
+	private async void OnInitialized(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+	{
+		if (!browser.IsBrowserInitialized)
+			return;
+
+		browser.Address = config?.MainPage ?? "https://www.whatsmyip.org/";
+
+		// 'Page.addScriptToEvaluateOnNewDocumentAsync' CDP 커맨드가 'FrameLoadStart' 이벤트 받고 'ExecuteScriptAsync' 호출하는 것보다 훨씬 더 확실함.
+		// 'FrameLoadStart' 이벤트 받자마자 스크립트 실행 등록시키더라도, 스크립트가 큐에 등록되고 전송되는 사이에 사이트 측 스크립트가 먼저 실행되어 버릴 위험성이 있음.
+		// 그러나, 'Page.addScriptToEvaluateOnNewDocumentAsync'는 크롬 자체 지원 기능이기도 하고, 무엇보다 사이트의 스크립트가 로드되기 전에 실행되는 것을 보장함. (https://chromedevtools.github.io/devtools-protocol/tot/Page/#method-addScriptToEvaluateOnNewDocument)
+		browser.GetDevToolsClient().Page.EnableAsync();
+		browser.GetDevToolsClient().Page.AddScriptToEvaluateOnNewDocumentAsync(nameRandom.ApplyTo(LibResources.namespaceInitJs + ';' + CefSharpResources.communicatorJs + ';' + LibResources.mainHelperJs));
+		// browser.GetDevToolsClient().Page.AddScriptToEvaluateOnNewDocumentAsync(nameRandom.ApplyTo(LibResources.namespaceInitJs + ';' + LibResources.mainHelperJs + ';' + CefSharpResources.communicatorJs)); // TODO: try this.
+
+		await EnableAdBlockingAsync();
+		LibLogger.Info<CefSharpBrowser>("ChromiumWebBrowser initialized.");
+	}
+
+	private async Task EnableAdBlockingAsync()
+    {
+        var devToolsClient = browser.GetDevToolsClient();
+
+        // 네트워크 요청을 추적하기 위해 Network.enable 호출
+        var enableParams = new Dictionary<string, object>();
+        await devToolsClient.ExecuteDevToolsMethodAsync("Network.enable", enableParams);
+
+        // 광고 차단 기능 활성화
+        var blockAdsParams = new Dictionary<string, object>
+		{
+			["urls"] = new string[]
+			{
+				"*://*.doubleclick.net/*",
+				"*://*.adservice.google.com/*",
+				"*://*.googlesyndication.com/*",
+				"*://*.googleadservices.com/*",
+				"*://*.adsafeprotected.com/*"
+			}
+		};
+        await devToolsClient.ExecuteDevToolsMethodAsync("Network.setBlockedURLs", blockAdsParams);
+    }
+
+	public void OnWebSocketSend(object? sender, JavaScriptBindingObject.WebSocketJsonMessageEventArgs args)
+	{
+		Task.Run(() =>
+		{
+			try
+			{
+				WebSocketMessage?.Invoke(this, new WebSocketMessageEventArgs(Guid.Empty, false, args.Json));
+			}
+			catch (Exception ex)
+			{
+				LibLogger.Error<CefSharpBrowser>(ex, "Handling WebSocket send event error. Message: {msg}", args.Json);
+			}
+		});
+	}
+
+	public void OnWebSocketReceive(object? sender, JavaScriptBindingObject.WebSocketJsonMessageEventArgs args)
+	{
+		Task.Run(() =>
+		{
+			try
+			{
+				WebSocketMessage?.Invoke(this, new WebSocketMessageEventArgs(Guid.Empty, true, args.Json));
+			}
+			catch (Exception ex)
+			{
+				LibLogger.Error<CefSharpBrowser>(ex, "Handling WebSocket receive event error. Message: {msg}", args.Json);
+			}
+		});
+	}
+
+	public void OnFrameLoadEnd(object? sender, FrameLoadEndEventArgs args)
+	{
+		LibLogger.Verbose<CefSharpBrowser>("CefSharp frame loaded: {url}", args.Url);
+		PageLoaded?.Invoke(sender, new PageLoadedEventArgs(args.Url));
+	}
+
+	public void OnLoadError(object? sender, LoadErrorEventArgs args) => PageError?.Invoke(sender, new PageErrorEventArgs(args.ErrorText, args.FailedUrl));
+
+	public override void Load(string url) => browser.LoadUrl(url);
+
+	public override void ShowDevTools()
+	{
+		browser.ShowDevTools();
+	}
+
+	public override void ExecuteJavaScript(string script, string? errorMessage = null)
+	{
+		try
+		{
+			if (browser.CanExecuteJavascriptInMainFrame)
+				browser.GetMainFrame()?.ExecuteJavaScriptAsync(script);
+		}
+		catch (Exception ex)
+		{
+			LibLogger.Error<CefSharpBrowser>(ex, errorMessage ?? "JavaScript execution error");
+		}
+	}
+
+	public override async Task<object?> EvaluateJavaScriptRawAsync(string script)
+	{
+		if (!browser.CanExecuteJavascriptInMainFrame)
+		{
+			LibLogger.Warn<CefSharpBrowser>("Trying to execute JavaScript before mainframe initialization finished.");
+			return null;
+		}
+
+		var frame = browser.GetMainFrame();
+		if (frame is null)
+			throw new InvalidOperationException("MainFrame is null");
+		var task = frame.EvaluateScriptAsync(script, timeout: TimeSpan.FromSeconds(1));
+		await task.WaitAsync(TimeSpan.FromSeconds(1));
+		var response = await task; // Will be finished immediately because the task had already finished @ L248
+		return response.Result;
+	}
+
+	public override IntPtr GetWindowHandle() => browser.GetBrowserHost().GetWindowHandle();
+
+	public override void SetFocus()
+	{
+		//browser.Focus();
+		browser.GetBrowserHost().SetFocus(true);
+		browser.GetBrowserHost().SendFocusEvent(true);
+	}
+}
---
+++ AutoKkutuLib.CefSharp/Resources/communicatorJs.js
@@ -0,0 +1,24 @@
+﻿/* communicatorJs : AutoKkutu - CefSharp JSB communicator
+ * 
+ * Reserved names:
+ * ___jsbGlobal___
+ * ___jsbObj___
+ * onSend
+ * onReceive
+ * bindObjectAsync
+ * BindObjectAsync
+ * ___commSend___
+ * ___commRecv___
+ */
+
+___commSend___ = (async function (msg) {
+    let fn = window['___jsbGlobal___']['bindObjectAsync'] ?? window['___jsbGlobal___']['BindObjectAsync'];
+    await fn('___jsbObj___');
+    window['___jsbObj___']['onSend'](msg)
+});
+
+___commRecv___ = (async function (msg) {
+    let fn = window['___jsbGlobal___']['bindObjectAsync'] ?? window['___jsbGlobal___']['BindObjectAsync'];
+    await fn('___jsbObj___');
+    window['___jsbObj___']['onReceive'](msg)
+});
